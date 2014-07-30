8f1c8abce9e232696368a6303f3e444c57a7eaab
Melissa De Bartolomeo <debartolomeo@hotmail.com>
2014-07-30 17:07:54 -0500

    added more sane comments to database getters/setters

--- includes/cc-aha-database-bridge.php
+++ includes/cc-aha-database-bridge.php
@@ -39,58 +39,68 @@ function cc_aha_get_form_data( $metro_id, $page = 1 ){
 
 	global $wpdb;
 	 
-	 //so we will return some data for the moment
-	$table_name = "wp_aha_assessment_board";
-	if( $wpdb->get_var("SHOW TABLES LIKE '$table_name'" ) != $table_name) {
-		return array( 	
-				'1.2.1.1' => 1,
-				'1.2.1.2' => 0, 
-				'1.2.1.3' => 'Saved text box stuff',
-				'1.2.2.1' => '23.45',
-				'2.1.1.1' => 1,
-				'2.1.1.2' => 1,
-				'2.1.1.3' => 0,
-				);
-	} else {
-		$form_rows = $wpdb->get_results( 
-			$wpdb->prepare( 
-			"
-			SELECT * 
-			FROM $wpdb->aha_assessment_board
-			WHERE BOARD_ID = %s
-			",
-			$metro_id )
-			, ARRAY_A
-		);
-		//print_r( $form_rows );
-		return $form_rows;
-	}
+	//get board data from database
+	//$table_name = "wp_aha_assessment_board";
+	$form_rows = $wpdb->get_results( 
+		$wpdb->prepare( 
+		"
+		SELECT * 
+		FROM $wpdb->aha_assessment_board
+		WHERE BOARD_ID = %s
+		",
+		$metro_id )
+		, ARRAY_A
+	);
+	//print_r( $form_rows );
+	return $form_rows;
 }
 
 /**
- * Updates board database with answers from survey.
+ * Updates board and school database tables with answers from survey.
+ *
+ * Takes $_POST arrays of [board] and [school] on form submit,
+ *	makes sure their values aren't null, false or empty (so we don't overwrite values
+ *	that weren't set in the survey) and wpdb->update s the respective table 
  *
  * @since    1.0.0
  * @param 	array
  * @return	
  */
 function cc_aha_update_form_data( ){
-	/*
-		Hmm, things to think about:
+	
+	/* $POST data, for development reference:
+	
+	[board] => Array
+        (
+            [2.2.2.1] => 1
+            [2.2.2.2] => Describing state promotion things.  at the local level.
+            [2.2.4.1] => local
+        )
 		
-		- we have a school table that will be updated differently than the board table:
-			- $_POST[board] will hold board key=>values
-			- $_POST[district][#] will hold distrit key=>values ?
-			
+	 [school] => Array
+        (
+            [4206550] => Array
+                (
+                    [2.2.5.1] => limited
+                    [2.2.5.1.1.1] => 
+                    [2.2.5.1.3] => textconewago
+                )
+
+            [4210710] => Array
+                (
+                    [2.2.5.1] => other
+                    [2.2.5.1.1.1] => 
+                    [2.2.5.1.3] => text for gettys
+                )
+			...
 	*/
 	
 	//Mel isn't sure if this is necessary?
 	if ( $_POST['metro_id'] != $_COOKIE['aha_active_metro_id'] ) return 0;
-	
 
-	//take metro id (BOARD_ID in this table) and update fields for which values are supplied in form)
 	global $wpdb;
 	
+	//get our board vars for the wpdb->update statement
 	$board_id = $_COOKIE['aha_active_metro_id']; // 'BOARD_ID' column in wp_aha_assessment_board; our WHERE clause
 	$board_table_name = $wpdb->aha_assessment_board;
 	$board_where = array( 
@@ -101,37 +111,40 @@ function cc_aha_update_form_data( ){
 	$update_board_data = array();
 	$update_board_data = $_POST['board'];
 	
-	//remove null values from update_board_data - will not go into database
+	//remove null, false and empty values from update_board_data - will not go into database
 	//Mel note: is this the correct handling of this data?  Don't overwrite on null form values?
 	$update_board_data_notempty = array();
 	$update_board_data_notempty = array_filter($update_board_data, "strlen");  //strlen as callback will remove false, empty and null but leave 0
 	
-	//get values for wpdb->update statement..
+	//if we have [board] values set by the form, update the table
 	if ( !empty ( $update_board_data_notempty ) ) {
 		$num_board_rows_updated = $wpdb->update( $board_table_name, $update_board_data_notempty, $board_where, $format = null, $where_format = null );
 	}
 	
-	//Now for school data
+	//get our school vars for the wpdb->update statement
 	$school_table_name = $wpdb->aha_assessment_school;
 	
 	//get key => value pairs for $_POST['school']!
 	$update_school_data = array();
 	$update_school_data = $_POST['school'];
 	
-	//foreach district in survey 
+	//foreach district in survey, update db
 	foreach ( $update_school_data as $key => $value ){
 		
 		$district_id = $key;
 		
+		//set where clause with this district and board
 		$school_where = array(
 			'AHA_ID' => $board_id,
 			'DIST_ID' => $district_id
 		);
 		
+		//the array in value is the district-specific data
 		$update_school_dist_data = $value;
+		$update_school_dist_data_notempty();
 		$update_school_dist_data_notempty = array_filter($update_school_dist_data, "strlen");
 		
-		$towrite .= $district_id; 
+		//update the table for this district
 		$num_school_rows_updated = $wpdb->update( $school_table_name, $update_school_dist_data_notempty, $school_where, $format = null, $where_format = null );
 	
 	}
@@ -146,16 +159,16 @@ function cc_aha_update_form_data( ){
 	//$towrite .= print_r($update_board_data_notempty, TRUE);
 	$towrite .= print_r($update_school_dist_data_notempty, TRUE);
 	//$towrite .= sizeof($update_board_data_notempty);
-	$fp = fopen("c:\\xampp\\logs\\aha_error_log.txt", 'a');
+	$fp = fopen("c:\\xampp\\logs\\aha_log.txt", 'a');
 	fwrite($fp, $towrite);
 	fclose($fp);
 	
 	
 	//will have to account for school data getting updated as well
-	if ( $num_board_rows_updated === FALSE ) {
-		return false;
+	if ( $num_board_rows_updated === FALSE || $num_school_rows_updated === FALSE  ) {
+		return false; //we have a problem updating
 	} else {
-		return $num_board_rows_updated; //num rows on success (wpdb->update returns 0 if no data change), false on no success 
+		return ( $num_board_rows_updated ); //num rows on success (wpdb->update returns 0 if no data change), FALSE on no success 
 	}
 
 }
@@ -171,81 +184,18 @@ function cc_aha_get_school_data( $metro_id ){
 	global $wpdb;
 	
 	//so we will return some data for the moment
-	$table_name = "wp_aha_assessment_school";
-	if( $wpdb->get_var("SHOW TABLES LIKE '$table_name'" ) != $table_name) {
-		return array( 	
-				0 => array(
-					'id' => 4206550,
-					'DIST_NAME' => 'CONEWAGO VALLEY SD',
-					'rank' => 1,
-					'2.1.4.1.1' => 0, // Elementary school PE requirements
-					'2.1.4.1.2' => 1, // Middle school PE requirements
-					'2.1.4.1.3' => 0, // High school PE requirements
-					'3.1.3.1.0' => 0, // Is there a school nutrition policy in place?
-					),				
-				1 => array(
-					'id' => 4210710,
-					'DIST_NAME' => 'GETTYSBURG AREA SD',
-					'rank' => 2,
-					'2.1.4.1.1' => 1, // Elementary school PE requirements
-					'2.1.4.1.2' => 1, // Middle school PE requirements
-					'2.1.4.1.3' => 0, // High school PE requirements
-					'3.1.3.1.0' => 1, // Is there a school nutrition policy in place?
-					'3.1.3.1.1' => 0,
-					'3.1.3.1.2' => 1,
-					'3.1.3.1.3' => 1,
-					'3.1.3.1.4' => 'thingy.com/policy?id=90&duh=2',
-
-					), 
-				2 => array(
-					'id' => 4206545,
-					'name' => 'LITTLESTOWN AREA SD',
-					'rank' => 3,
-					'2.1.4.1.1' => 0, // Elementary school PE requirements
-					'2.1.4.1.2' => 0, // Middle school PE requirements
-					'2.1.4.1.3' => 0, // High school PE requirements
-					'3.1.3.1.0' => 1, // Is there a school nutrition policy in place?
-					'3.1.3.1.1' => 1,
-					'3.1.3.1.2' => 0,
-					'3.1.3.1.3' => 0,
-					'3.1.3.1.4' => 'thingy.com/policy?id=90&duh=2',
-					),
-				3 => array(
-					'id' => 4203450,
-					'DIST_NAME' => 'BERMUDIAN SPRINGS SD',
-					'rank' => 4,
-					'2.1.4.1.1' => 0, // Elementary school PE requirements
-					'2.1.4.1.2' => 0, // Middle school PE requirements
-					'2.1.4.1.3' => 1, // High school PE requirements
-					'3.1.3.1.0' => 0, // Is there a school nutrition policy in place?
-					), 
-				4 => array(
-					'id' => 4224300,
-					'DIST_NAME' => 'UPPER ADAMS SD',
-					'rank' => 5,
-					'2.1.4.1.1' => 0, // Elementary school PE requirements
-					'2.1.4.1.2' => 0, // Middle school PE requirements
-					'2.1.4.1.3' => 0, // High school PE requirements
-					'3.1.3.1.0' => 1, // Is there a school nutrition policy in place?
-					'3.1.3.1.1' => 1,
-					'3.1.3.1.2' => 0,
-					'3.1.3.1.3' => 1,
-					'3.1.3.1.4' => 'thingy.com/policy?id=90&duh=2',
-					),
-				);
-	} else {
-		$form_rows = $wpdb->get_results( 
-			$wpdb->prepare( 
-			"
-			SELECT * 
-			FROM $wpdb->aha_assessment_school
-			WHERE AHA_ID = %s
-			",
-			$metro_id )
-			, ARRAY_A
-		);
-		//print_r( $form_rows );
-		return $form_rows;
-	}
+	//$table_name = "wp_aha_assessment_school";
+	$form_rows = $wpdb->get_results( 
+		$wpdb->prepare( 
+		"
+		SELECT * 
+		FROM $wpdb->aha_assessment_school
+		WHERE AHA_ID = %s
+		",
+		$metro_id )
+		, ARRAY_A
+	);
+	//print_r( $form_rows );
+	return $form_rows;
 }
 
