16767e44d5f15ae6208f8ff93e8c1c092d2c5da3
Melissa De Bartolomeo <debartolomeo@hotmail.com>
2015-01-26 17:40:23 -0600

    trying the gform_entry_id_pre_save_lead hook...

--- includes/cc-ohio-chc-gf-functions.php
+++ includes/cc-ohio-chc-gf-functions.php
@@ -63,19 +63,21 @@ function cc_ohio_add_year($value){
     return date("Y");
 }
 
+
+//interrupt all gravity forms to check for ohio forms and pre-existing entries by county
+add_filter("gform_pre_render", 'cc_ohio_populate_by_existing');
+
 /* Populate Form by Existing entry
  * 
  * sort of...from here: http://pastie.org/1917904
  * http://www.gravityhelp.com/forums/topic/need-help-creating-an-edit-form
+ *
  * @params form object, entry object 
  * @returns form object
  *
  */
 
-//TODO: make this 
-add_filter("gform_pre_render", 'populate_by_existing');
-
-function populate_by_existing( $form ){
+function cc_ohio_populate_by_existing( $form ){
 
 	//which forms to care about? Get all Ohio county-input forms
 	$form_array = cc_ohio_chc_get_gf_forms_all();
@@ -88,6 +90,7 @@ function populate_by_existing( $form ){
 	//TODO: account for admin here...or at top of function or something
 	$current_county = get_user_county();
 	$county_field_name = "cc_ohio_county";
+	$old_entry_num_field_name = "cc_ohio_update_entry_id";
 	
 	//get the form object, to prepopulate
 	$gf_form_num = $form["id"];
@@ -95,12 +98,13 @@ function populate_by_existing( $form ){
 	
 	//for this form, what field id is the county?
 	$county_field_id = get_gf_field_id_by_label( $form, $county_field_name );
-	//var_dump( $county_field_id);
+	$old_entry_num_field_id = get_gf_field_id_by_label( $form, $old_entry_num_field_name );
+
 
 	
 	$entry = cc_ohio_chc_get_county_entry_by_form_number( $gf_form_num );
 	$entry = $entry[0]; //why you know work current()?
-	
+	$entry_id = (int)$entry['id']; 
 	
 	//forms have $form['field']['id'], but entries have $entry[#] where 'id' = #
     foreach($form['fields'] as &$field){
@@ -111,61 +115,91 @@ function populate_by_existing( $form ){
 				$field['defaultValue'] = $current_county;
 			}
 			continue;
-		}
-	
-        switch($field['type']){
-        case 'checkbox':
-
-            $i = 1;
-            foreach($field['choices'] as &$choice){
 			
+		} else { //we have an entry
+			
+			switch($field['type']){
+			case 'checkbox':
+
+				$i = 1;
+				foreach($field['choices'] as &$choice){
+				
+					$current_field_id = (string)$field['id'];
+					
+					if(!isset( $entry[$current_field_id . '.' . $i])) {
+						$i++;   
+						continue;
+					}
+
+					if($choice['value'] == $entry[$current_field_id . '.' . $i])
+						$choice['isSelected'] = true;
+
+					$i++;
+				}
+				break;
+
+			case 'address': //not right now
+				foreach($field['inputs'] as &$input){
+					$current_field_id = (string)$field['id'];
+
+					$field['allowsPrepopulate'] = true;
+					$parameter_name = strtolower(str_replace(' ', '_', str_replace('/ ', '', $input['label'])));
+
+					$input['name'] = $parameter_name;
+					$input_value = rgar($entry, (string) $input['id']);
+
+					add_filter('gform_field_value_' . $parameter_name, create_function("", "return '$input_value';"));
+
+				}
+				break;
+
+			default:
+
 				$current_field_id = (string)$field['id'];
 				
-                if(!isset( $entry[$current_field_id . '.' . $i])) {
-                    $i++;   
-                    continue;
-                }
-
-                if($choice['value'] == $entry[$current_field_id . '.' . $i])
-                    $choice['isSelected'] = true;
-
-                $i++;
-            }
-            break;
-
-        case 'address': //not right now
-            foreach($field['inputs'] as &$input){
-				$current_field_id = (string)$field['id'];
-
-                $field['allowsPrepopulate'] = true;
-                $parameter_name = strtolower(str_replace(' ', '_', str_replace('/ ', '', $input['label'])));
-
-                $input['name'] = $parameter_name;
-                $input_value = rgar($entry, (string) $input['id']);
-
-                add_filter('gform_field_value_' . $parameter_name, create_function("", "return '$input_value';"));
-
-            }
-            break;
-
-        default:
-
-			$current_field_id = (string)$field['id'];
-			
-			//var_dump( (string)$current_field_id);
-			//var_dump( $entry[$current_field_id]);
-			//var_dump( $entry[5]);
-			
-			//check to see if there's a value for the current field in the $entry object
-			if( !isset( $entry[$current_field_id] ) ){
-				continue; 
-			} else {
-				$field['defaultValue'] = $entry[$current_field_id];   
+				//var_dump( (string)$current_field_id);
+				//var_dump( $entry[$current_field_id]);
+				//var_dump( $entry[5]);
 				
+				//check to see if there's a value for the current field in the $entry object
+				if( !isset( $entry[$current_field_id] ) ){
+					continue; 
+				} else if ( $field['id'] == $old_entry_num_field_id ) { //we're on the old entry number field
+					$field['defaultValue'] = $entry_id;
+				} else {
+					$field['defaultValue'] = $entry[$current_field_id];   
+					
+				}
 			}
-        }
-
+		}
     }
 
     return $form;
+}
+
+//interrupt all gravity forms saving, check for our forms
+add_filter( 'gform_entry_id_pre_save_lead', 'cc_ohio_update_entry_on_form_submission', 10, 2 );
+
+/*
+ * Interrupts the gform saving to allow us to update an entry by id, rather than create a new one
+ *
+ */
+function cc_ohio_update_entry_on_form_submission( $entry_id, $form ) {
+
+	//var_dump( $entry_id );
+	//which forms to care about? Get all Ohio county-input forms
+	$form_array = cc_ohio_chc_get_gf_forms_all();
+	
+	//are we dealing w the ohio forms?  If not, return the normal form
+	//if( !(in_array( $form["id"], $form_array ) ) ) {
+		//return $entry_id;
+	//}
+
+	$update_entry_id = rgpost( 'cc_ohio_update_entry_id' );
+	//$update_entry_id = rgpost( );
+	
+	var_dump( $update_entry_id );
+	return $update_entry_id ? (int)$update_entry_id : $entry_id;
+	
+	
 }
